ecb2d478998c117505f66f05393daf887d8c57cc
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-15 19:14:16 +0300

    Feat: Authentication code added and html forms attached

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from config import config_options, DevConfig
+from config import config_options, DevConfig 
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 def create_app(config_name):
 
     app = Flask(__name__, static_url_path='/app/static/')
+
+    #authentication
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
@@ -16,6 +25,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Will add the views and forms
      # Registering the blueprint
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from flask_login import login_required
 
 # Views
 @main.route('/')
@@ -17,3 +18,9 @@ def snippet():
     View root page function that returns the index page and its data
     '''
     return render_template('snippet.html')
+
+@main.route ('/blog/new/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def new_blog(id):
+    pass
+
