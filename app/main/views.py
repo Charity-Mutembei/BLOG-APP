fa7be28c2946e27d7797ed5dd4919c5c114738d3
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-16 16:34:30 +0300

    Feat: profile page and login authentiction codeinstalled and tested its working

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for, abort
 from . import main
 from flask_login import login_required
+from ..models import User
 
 # Views
 @main.route('/')
@@ -18,6 +19,18 @@ def snippet():
     View root page function that returns the index page and its data
     '''
     return render_template('snippet.html')
+
+
+@main.route('/user/<uname>')
+def profile (uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+
+
+    return render_template('profile/profile.html', user = user)
 
 @main.route ('/blog/new/<int:id>', methods = ['GET', 'POST'])
 @login_required
